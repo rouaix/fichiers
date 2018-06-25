d943b5156d8debafacc5f2a6bbc47e66e36c9645
rouaix <daniel@rouaix.com>
2018-06-25 22:05:41 +0200

    Add files via upload

--- MySql.sql
+++ MySql.sql
@@ -1,18 +1,18 @@
 Requête 1
 use webacademie;
-show tables;
+show tables
 
 Requête 2
-describe webacademie.film;
+describe webacademie.film
 
 Requête 3
-select titre as "titre film"  from webacademie.film;
+select titre as "titre film"  from webacademie.film
 
 Requête 4
-select * from webacademie.film where id_genre is null;
+select * from webacademie.film where id_genre is null
 
 Requête 5
-select * from webacademie.film where date_debut_affiche between'2000-01-01' and '2000-12-31' order by date_debut_affiche desc limit 10;
+select * from webacademie.film where date_debut_affiche between'2000-01-01' and '2000-12-31' order by date_debut_affiche desc limit 10
 
 Requête 6
 select * from webacademie.film where (id_genre=1 or id_genre = 2) and titre LIKE 'a%'
@@ -33,13 +33,10 @@ Requête 11
 select count(id_abo) as "nbr_abo",round(avg (prix),-1) as "avg abo" from webacademie.abonnement
 
 Requête 12
-select id_film as "max id film" from film where id_genre =8 and date_debut_affiche BETWEEN '1980-01-01' and '1999-12-31' order by film.id_film DESC LIMIT 1;
+select id_film as "max id film" from film where id_genre =8 and date_debut_affiche BETWEEN '1980-01-01' and '1999-12-31' order by film.id_film DESC LIMIT 1
 
 Requête 13
 select id_membre as "id_membre", date_dernier_film as "last movie" from webacademie.membre where membre.id_dernier_film = '2557' or membre.id_dernier_film = '2741' order by date_dernier_film desc
 
 Requête 14
-select film.id_genre, genre.nom, count(film.id_film) from film join genre ON film.id_genre = genre.id_genre where film.id_genre between '1' and '3' group by film.id_genre
-
-
-
+select film.id_genre, genre.nom, count(film.id_film) from film left join genre ON film.id_genre = genre.id_genre where film.id_genre between '1' and '3' group by film.id_genre
